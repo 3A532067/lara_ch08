50431475dc20822c589c2590e1af33dd612bba05
3A532067 <s3a532067@student.ncut.edu.tw>
2018-11-20 18:31:32 +0800

    練習3-2:建立三個資料庫之間的關係 & 在 Score 資料庫當中建立兩個 scope 方法，用以對查詢的資料作排序

--- app/Student.php
+++ app/Student.php
@@ -3,8 +3,20 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use \App\User as UserEloquent;
+use \App\Score as ScoreEloquent;
 
+use DB;
 class Student extends Model
 {
     //
+
+    protected $table = 'student';
+
+    public function user(){
+        return $this->belongsTo(UserEloquent::class);
+    }
+    public function score(){
+        return $this->hasOne(ScoreEloquent::calss);
+    }
 }
--- app/User.php
+++ app/User.php
@@ -6,9 +6,11 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
+use \App\Student as StudentEloquent;
+
 class User extends Authenticatable
 {
-    use Notifiable;
+
 
     /**
      * The attributes that are mass assignable.
@@ -27,4 +29,8 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function student(){
+        return $this->hasOne(StudentEloquent::class);
+    }
 }
