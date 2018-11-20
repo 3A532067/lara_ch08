cdc16349aca15759a50dc06659fafa49bd4fe8a1
3A532067 <s3a532067@student.ncut.edu.tw>
2018-11-20 20:32:44 +0800

    練習4:修改 board 視圖&修改 BoardController

--- app/Http/Controllers/BoardController.php
+++ app/Http/Controllers/BoardController.php
@@ -4,10 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Http\Requests;
+
+use App\Score as ScoreEloquent;
+
+use Route;
+use View;
+
 class BoardController extends Controller
 {
     public function getIndex()
     {
-        return view('board');
+        return view('board',['scores'=>ScoreEloquent::with
+        ('student')->orderByTotal()->orderBySubject()->get()]);
+    }
+    public function getName(){
+        return Route::currentRouteAction();
     }
 }
